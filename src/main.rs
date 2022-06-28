6a40f398fe5ee12cc7503a555b0962bf344ca256
KRaken <kuzembay@gmail.com>
2022-06-28 20:05:03 +0600

    feat: added basic routes

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,18 @@
-fn main() {
-    println!("Hello, world!");
+mod api;
+mod models;
+mod repository;
+
+#[macro_use]
+extern crate rocket;
+
+use api::user_api::{create_user, get_user};
+use repository::mongodb_repo::MongoRepo;
+
+#[launch]
+fn rocket() -> _ {
+    let db = MongoRepo::init();
+    rocket::build()
+        .manage(db)
+        .mount("/", routes![create_user])
+        .mount("/", routes![get_user])
 }
